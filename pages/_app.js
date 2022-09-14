4a37276c38a76bebf5e2bf744ca39f22a5d80b0b
zohaibzyebi <zyebi553@gmail.com>
2022-09-14 16:59:25 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import '../styles/fonts.css';
 import '../styles/globals.css';
 import Layout from '../pages/layout';
-import { LicenseInfo } from '@mui/x-data-grid-pro';
 
 
-LicenseInfo.setLicenseKey('YOUR_LICENSE_KEY');
 
 function MyApp({ Component, pageProps }) {
   return (
